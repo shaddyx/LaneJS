9db445331efab08b7301d51905732ccb2f0bda2f
Igor Khotin <khotin@gmx.com>
2014-04-15 15:01:28 +0300

    [*] updated run script to be compatible with different browsers

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-google-chrome index.html
+url=index.html
+
+[[ -x $BROWSER ]] && exec "$BROWSER" "$url"
+path=$(which xdg-open || which gnome-open) && exec "$path" "$url"
+echo "Can't find browser!"
