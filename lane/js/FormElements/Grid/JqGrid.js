d793d90c2b521bf12d0a7f03826e0b77650e720f
Anatoly Yakushko <ayakushko@intropro.com.ua>
2014-05-30 13:34:20 +0300

    removed unused comments
    --HG--
    branch : TimeGraph

--- lane/js/FormElements/Grid/JqGrid.js
+++ lane/js/FormElements/Grid/JqGrid.js
@@ -32,21 +32,6 @@ JqGrid.prototype.afterDraw = function(){
 	   	colModel:colModel,
 	   	multiselect: this._values.multiSelect
 	});
-	
-	
-	/*var mydata = [
-			{id:"1",invdate:"2007-10-01",name:"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-			{id:"2",invdate:"2007-10-02",name:"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-			{id:"3",invdate:"2007-09-01",name:"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-			{id:"4",invdate:"2007-10-04",name:"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-			{id:"5",invdate:"2007-10-05",name:"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-			{id:"6",invdate:"2007-09-06",name:"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"},
-			{id:"7",invdate:"2007-10-04",name:"test",note:"note",amount:"200.00",tax:"10.00",total:"210.00"},
-			{id:"8",invdate:"2007-10-03",name:"test2",note:"note2",amount:"300.00",tax:"20.00",total:"320.00"},
-			{id:"9",invdate:"2007-09-01",name:"test3",note:"note3",amount:"400.00",tax:"30.00",total:"430.00"}
-		];
-	for(var i=0;i<=mydata.length;i++)
-		jQuery(this._tableElement).jqGrid('addRowData',i+1,mydata[i]);*/
 };
 
 
