db538bdf9e62101d575165181fed453653bb3027
Anatolii Yakushko [Intropro] <AYakushko@intropro.com.ua>
2014-12-01 18:43:13 +0200

    [+] added Iframe component

---
+++ lane/js/FormElements/Iframe.js
@@ -0,0 +1,17 @@
+var Iframe = function() {
+	FormElement.call(this);
+};
+Util.extend(Iframe, FormElement);
+Iframe.type = "Iframe";
+Iframe.prototype.html = Iframe.addProperty("src","",  {type:"string"});
+Iframe.prototype.updateSrc = function(){
+	if (this._v.isDrawn){
+		this._elements.iFrame.htmlInnerElement.src = this._v.src;
+	}
+};
+
+Iframe.on("afterDraw", function(){
+	this.updateSrc();
+});
+
+Iframe.on("srcChanged", Iframe.prototype.updateSrc);
---
+++ lane/js/skins/Iframe.js
@@ -0,0 +1,18 @@
+/*
+ * Box element properties
+ * @@@dependsOn: BoxElement
+ * @@@dependsOn: FormElement
+ * @@@nameSuffix:Skin
+ */
+var IframeSkin = {};
+IframeSkin.def = {
+	c : [ {
+		hs:true,
+		vs:true,
+		params : {
+			tag : "iframe",
+			css : 'background-color:white;padding:0;border:0;display:block !important; '
+		},
+		name:"iFrame"
+	} ]
+};
